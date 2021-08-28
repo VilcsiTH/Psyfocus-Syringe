8ea8ca33c087d25d9cf75c3e2e27d54f3a3ec889
VilcsiTH <vilcsith@gmail.com>
2021-08-28 21:12:39 +0200

    Add project files.

---
+++ Source/SyringesEffects/Effects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace SyringesEffects
+{
+	public class CompUseEffect_RefillPsyfocus : CompUseEffect
+	{
+		public override void DoEffect(Pawn usedBy)
+		{
+			Pawn_PsychicEntropyTracker psychicEntropy = usedBy.psychicEntropy;
+			if (psychicEntropy == null)
+			{
+				return;
+			}
+			psychicEntropy.OffsetPsyfocusDirectly(1.0f);
+		}
+	}
+}
