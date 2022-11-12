45f3bc394458c4813d5a0a10e2589b06a73cdd68
VilcsiTH <vilcsith@gmail.com>
2022-11-12 11:10:13 +0100

    Add versioning, update for 1.4

---
+++ 1.4/Source/SyringesEffects/Effects.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace SyringesEffects
+{
+	public class CompUseEffect_RefillPsyfocus : CompUseEffect
+	{
+		public override void DoEffect(Pawn usedBy)
+		{
+			Pawn_PsychicEntropyTracker psychicEntropy = usedBy.psychicEntropy;
+			if (psychicEntropy == null)
+			{
+				return;
+			}
+			psychicEntropy.OffsetPsyfocusDirectly(1.0f);
+		}
+	}
+	public class CompUseEffect_EmptyNeuralHeat : CompUseEffect
+	{
+		public override void DoEffect(Pawn usedBy)
+		{
+			((Pawn)usedBy).psychicEntropy.TryAddEntropy(-99999f, null, true, false);
+		}
+	}
+}
